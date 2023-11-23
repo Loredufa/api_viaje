dacf7518f7004b4af7d84ef1bd7bf8ea9e53a512
LoreDufa <lorenadufaur@gmail.com>
2023-11-23 17:43:40 -0300

    cambio campo a text

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize) {
       allowNull: false,
     },
     texto_gral: {
-      type:DataTypes.STRING,
+      type:DataTypes.TEXT,
       allowNull:true,
     }
   })
