cbc1b662f5dc2ee2b49846191d034a90800d54ac
LoreDufa <lorenadufaur@gmail.com>
2024-04-05 10:44:33 -0300

    reinicio

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use('/', routes);
 
 
 //Servidor
-conn.sync({force:false}).then(() => {
+conn.sync({force:true}).then(() => {
   console.log('Base de datos conectada')
   app.listen(PORT, () => {
     createEmoji()
